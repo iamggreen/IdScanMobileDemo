a7828317153b547afc095f9316affbf47ed66c58
Gary Green <iamggreen@gmail.com>
2018-01-24 22:48:51 -0600

    Created main page with QR code and mobile camera page"

---
+++ IdScanMobileDemo/Controllers/SessionController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using IdScanMobileDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdScanMobileDemo.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Index(Guid? sessionId)
+        {
+            if (!IsValidSessionId(sessionId))
+            {
+                return HandleInvalidSessionId();
+            }
+
+            return View(new SessionModel(sessionId.Value));
+        }
+
+        public IActionResult Camera(Guid? sessionId)
+        {
+            if (!IsValidSessionId(sessionId))
+            {
+                return HandleInvalidSessionId();
+            }
+
+            return View(new SessionModel(sessionId.Value));
+        }
+
+        private bool IsValidSessionId(Guid? sessionId)
+        {
+            return sessionId != null && sessionId.Value != Guid.Empty;
+        }
+
+        private IActionResult HandleInvalidSessionId()
+        {
+            return RedirectToAction(nameof(Index), new { sessionId = Guid.NewGuid() });
+        }
+    }
+}
---
+++ IdScanMobileDemo/Models/SessionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IdScanMobileDemo.Models
+{
+    public class SessionModel
+    {
+        public Guid SessionId { get; }
+
+        public SessionModel(Guid sessionId)
+        {
+            SessionId = sessionId;
+        }
+    }
+}
